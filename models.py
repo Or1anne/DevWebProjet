6eaaa43c080ad594074860df3a6ba9953b4f470b
Or1anne <133128206+Or1anne@users.noreply.github.com>
2025-03-20 10:13:45 +0100

    Restructuration

---
+++ models.py
@@ -0,0 +1,10 @@
+from . import db
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # clé primiaire obligatoire pour SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
